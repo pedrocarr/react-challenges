6736a9ffb45c06c8654dca65f9feda33ae9e0713
pedrocarr <eng.pedro.amb@gmail.com>
2025-08-14 12:12:52 +0200

    separation of concerns in react challenges

--- pokemon-list/src/main.tsx
+++ pokemon-list/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import PokemonList from './PokemonList.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PokemonList />
   </StrictMode>,
 )
