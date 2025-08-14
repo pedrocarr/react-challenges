f28e94d9d122603b39265f1eeaddd07160320205
pedrocarr <eng.pedro.amb@gmail.com>
2025-08-14 15:09:15 +0200

    add npm package to test it

--- custom-hooks/src/CustomHooksDemo.tsx
+++ custom-hooks/src/CustomHooksDemo.tsx
@@ -1,10 +1,14 @@
 import useCustom from "./useCustom"
-
+// @ts-ignore
+import wcc from "world-countries-capitals"
 const CustomHooksDemo = () => {
+
+
+  const example = wcc.getRandomCountry()
 
   const value = useCustom()
   return (
-    <div>{value}</div>
+    <div>{value} - {example}</div>
   )
 }
 
